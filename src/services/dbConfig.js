82ca1a79ecd26226862197301a64b555d3cf1c39
thanhtran17 <enigma15012001@gmail.com>
2022-08-03 12:08:18 +0700

    remove test db

--- src/services/dbConfig.js
+++ src/services/dbConfig.js
@@ -2,18 +2,10 @@ const mysql = require('mysql');
 
 const config = mysql.createConnection({
   host: '37.59.55.185',
-  user: '9qGuH1yAkW',
-  password: 'Jpje0mbY0u',
+  user: 'ptbID8hnJu',
+  password: 'NoWnWCoTyp',
   port: 3306,
-  database: '9qGuH1yAkW',
+  database: 'ptbID8hnJu',
 });
-
-// const config = mysql.createConnection({
-//   host: '37.59.55.185',
-//   user: 'ptbID8hnJu',
-//   password: 'NoWnWCoTyp',
-//   port: 3306,
-//   database: 'ptbID8hnJu',
-// });
 
 module.exports = config;
