3b235269430149d1d4d452c7eaec3dcf4274d464
thanhtran17 <enigma15012001@gmail.com>
2022-08-03 09:57:06 +0700

    reimport about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import classes from "./styles.module.scss";
-import { MemberCard } from "./MemberCard";
-import { Col, Row } from "antd";
-import avatar1 from "../../assets/avatar1.jpg";
-import avatar2 from "../../assets/avatar2.jpg";
-import avatar3 from "../../assets/avatar3.jpg";
-import avatar4 from "../../assets/avatar4.jpg";
+import React from 'react';
+import MemberCard from '../../components/common/MemberCard';
+import { Col, Row } from 'antd';
+import avatar1 from '../../assets/avatar1.jpg';
+import avatar2 from '../../assets/avatar2.jpg';
+import avatar3 from '../../assets/avatar3.jpg';
+import avatar4 from '../../assets/avatar4.jpg';
+
 const About = () => {
   const members = [
     {
-      fullname: "Tran Phuc Thanh",
-      id: "20194846",
+      fullname: 'Tran Phuc Thanh',
+      id: '20194846',
       avatar: avatar1,
-      role: "Leader",
+      role: 'Leader',
     },
     {
-      fullname: "Dinh Ngoc Khue",
-      id: "20194782",
+      fullname: 'Dinh Ngoc Khue',
+      id: '20194782',
       avatar: avatar2,
-      role: "Member",
+      role: 'Member',
     },
     {
-      fullname: "Le Dinh Huy",
-      id: "20194776",
+      fullname: 'Le Dinh Huy',
+      id: '20194776',
       avatar: avatar4,
-      role: "Member",
+      role: 'Member',
     },
     {
-      fullname: "Mai Anh Duc",
-      id: "20194740",
+      fullname: 'Mai Anh Duc',
+      id: '20194740',
       avatar: avatar3,
-      role: "Member",
+      role: 'Member',
     },
   ];
   return (
